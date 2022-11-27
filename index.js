d756e30055d0b333582f1f301d760df85ab7a989
mohamad-ridwan <mr643062@gmail.com>
2022-11-27 07:42:23 +0700

    ridwan1 new branch

--- index.js
+++ index.js
@@ -61,15 +61,15 @@ const uri = process.env.MONGO_DB_URI
 
 const PORT = process.env.PORT || 6300
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    // Listener
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+})
 
 const connection = mongoose.connection;
 
 connection.once('open', () => {
     console.log("MongoDB database connection has been established successfully.");
-})
-
-// Listener
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
 })
